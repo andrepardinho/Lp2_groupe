fa2db35c313b608a62ac368f9d5a006d05ce8599
andrepardinho <nandre670@gmail.com>
2024-09-11 11:44:27 -0300

    Exemplos att: Q01 a Q05

--- Lista2 rev/exemimg.c
+++ Lista2 rev/exemimg.c
@@ -4,6 +4,7 @@
 #include <time.h>
 
 int main() {
+
     // Inicializa o gerador de números aleatórios
     srand(time(NULL));
 
@@ -11,7 +12,7 @@ int main() {
     unsigned char linha[LARGURA];
     unsigned char img[ALTURA][LARGURA];
 
-    // Teste da função geraLinha com tipo = 0 (aleatório)
+    // QUESTAO 1: Teste da função geraLinha com tipo = 0 (aleatório)
     geraLinha(linha, 0);
     printf("Primeiros 10 valores de linha: ");
     for (int i = 0; i < 10; i++) {
@@ -19,7 +20,7 @@ int main() {
     }
     printf("\n");
 
-    // Teste da função geraImgGreyFull_R, preenchendo todos os pixeis com o valor predeterminado 100
+    // QUESTAO 2: Teste da função geraImgGreyFull_R, preenchendo todos os pixeis com o valor predeterminado 100
     geraImgGreyFull_R(img, 100);
     printf("Primeiros 10 valores de img após geraImgGreyFull_R: ");
     for (int i = 0; i < 10; i++) {
@@ -27,7 +28,7 @@ int main() {
     }
     printf("\n");
 
-    // Teste da função geraImgGreyB_R, retornando uma imagem preta
+    // QUESTAO 3: Teste da função geraImgGreyB_R, retornando uma imagem preta
     geraImgGreyB_R(img);
     printf("Primeiros 10 valores de img após geraImgGreyB_R: ");
     for (int i = 0; i < 10; i++) {
@@ -35,7 +36,7 @@ int main() {
     }
     printf("\n");
 
-    // Teste da função geraImgGreyW_R, retornando uma imagem branca
+    // QUESTAO 4: Teste da função geraImgGreyW_R, retornando uma imagem branca
     geraImgGreyW_R(img);
     printf("Primeiros 10 valores de img após geraImgGreyW_R: ");
     for (int i = 0; i < 10; i++) {
@@ -43,7 +44,7 @@ int main() {
     }
     printf("\n");
 
-    // Teste da função geraImgGrey, o parametro altera a probablilidade do tom do pixel.
+    // QUESTAO 5: Teste da função geraImgGrey, o parametro altera a probablilidade do tom do pixel.
     geraImgGrey(img, 1);
     printf("Primeiros 10 valores de img após geraImgGrey: ");
     for (int i = 0; i < 10; i++) {
@@ -54,55 +55,6 @@ int main() {
     return 0;
 }
 
-
-// int main() {
-//     srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios
-
-//     unsigned char img[ALTURA][LARGURA];
-
-//     unsigned char pixel = geraPixelCinza(0); // Gera um valor de pixel com probabilidade uniforme
-//     geraImgGreyFull_R(img, pixel);
-//     printf("Pixel [0][0] após geraImgGreyFull_R: %u\n", img[0][0]);
-//     printf("Pixel [%d][%d] após geraImgGreyFull_R: %u\n", ALTURA-1, LARGURA-1, img[ALTURA-1][LARGURA-1]);
-
-//     // Outros testes...
-
-//     return 0;
-// }
-
-
-/*
-// Q01 a Q05
-int main() {
-    srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios
-
-    unsigned char img[480][640];
-
-    // Q02: Testa a função geraImgGreyFull_R
-    unsigned char pixel = geraPixelCinza(0); // Gera um valor de pixel com probabilidade uniforme
-    geraImgGreyFull_R(img, pixel);
-    printf("Pixel [0][0] apos geraImgGreyFull_R: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGreyFull_R: %u\n", img[479][639]);
-
-    // Q03: Testa a função geraImgGreyB_R
-    geraImgGreyB_R(img);
-    printf("Pixel [0][0] apos geraImgGreyB_R: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGreyB_R: %u\n", img[479][639]);
-
-    // Q04: Testa a função geraImgGreyW_R
-    geraImgGreyW_R(img);
-    printf("Pixel [0][0] apos geraImgGreyW_R: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGreyW_R: %u\n", img[479][639]);
-
-    // Q05: Testa a função geraImgGrey
-    int tipo = 1;
-    geraImgGrey(img, tipo);
-    printf("Pixel [0][0] apos geraImgGrey: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGrey: %u\n", img[479][639]);
-
-    return 0;
-}
-*/
 
 
 /* 
